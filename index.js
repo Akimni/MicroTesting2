14ba75900687cb694efd3e489d9b6f9601bf2c64
Akimni <muhammadhakimhafidz@gmail.com>
2022-07-20 05:08:46 +0800

    commit

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express = require('express')
-const cors = require('cors')
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'qekmfhbqusidva',
-  host: 'ec2-52-72-56-59.compute-1.amazonaws.com',
-  database: 'dd29m58g7a4tda',
-  password: '22cff620d0b06f17950d4f4669a1e0f11f168c04053c0a218b1d83ee130fddb9',
-})
-
+const bodyParser = require('body-parser')
 const app = express()
-app.use(cors())
+const db = require('./queries')
+const port = 3000
 
-app.get('/displayaccount', (req, res) => {
-  /*var micro_username = req.query.username;
-
-  console.log("username: " + micro_username);*/
-
-  connect.getConnection(function (err, connection) {
-    if (err) { res.send('Error Database Connection') }
-    else {
-      var sql = "select * from khairatmember";
-      connect.query(sql, function (err, result) {
-        if (err) { throw err }
-        else {
-          res.send(result)
-        }
-      connection.release()
-      })
-    }
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
---
+++ queries.js
@@ -0,0 +1,15 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'qekmfhbqusidva',
+  host: 'ec2-52-72-56-59.compute-1.amazonaws.com',
+  database: 'dd29m58g7a4tda',
+  password: '22cff620d0b06f17950d4f4669a1e0f11f168c04053c0a218b1d83ee130fddb9',
+})
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM khairatmember', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
