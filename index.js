e510640ea6a6539ed44c40c68eec04c32dce897e
Akimni <muhammadhakimhafidz@gmail.com>
2022-07-20 04:45:36 +0800

    commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'qekmfhbqusidva',
   host: 'ec2-52-72-56-59.compute-1.amazonaws.com',
   database: 'dd29m58g7a4tda',
   password: '22cff620d0b06f17950d4f4669a1e0f11f168c04053c0a218b1d83ee130fddb9',
-})
+});
 
 const app = express();
 app.use(cors());
